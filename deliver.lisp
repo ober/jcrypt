6843c862663ce8a88ef1fd8af8df5c569fde80b2
Jaime Fournier <jaimef@linbsd.org>
2015-05-12 21:07:45 -0700

    make sbcl the same.

--- deliver.lisp
+++ deliver.lisp
@@ -1,13 +1,13 @@
 (load "load.lisp")
 
-#+(or ccl clisp sbcl)
+#+(or ccl clisp)
 (ql:quickload "trivial-dump-core")
 
 #+sbcl
-(sb-ext:save-lisp-and-die "main-sbcl" :compression 9 :executable t :toplevel 'jcrypt::main :save-runtime-options t)
+(sb-ext:save-lisp-and-die "jcrypt" :compression 9 :executable t :toplevel 'jcrypt::main :save-runtime-options t)
 
 #+(or ccl clisp)
 (trivial-dump-core:save-executable "jcrypt" #'jcrypt::main)
 
 #+lispworks
-(deliver 'jcrypt::main "jcrypt" 1 :multiprocessing t :keep-eval t)
+(deliver 'jcrypt::main "jcrypt" 4 :multiprocessing t :keep-eval t)
